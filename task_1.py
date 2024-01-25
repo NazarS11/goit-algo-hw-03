56593d0f95ecd9811c19b56b8e192e03b398ba7e
NazarS11 <bamperor@gmail.com>
2024-01-25 21:46:37 +0200

    v1.3

--- task_1.py
+++ task_1.py
@@ -1,13 +1,13 @@
 from datetime import datetime as dtdt
 
 def get_days_from_today(date):
-#приведення поточної дати до типу datetime "тільки дата"
+# converting the current date to the "date only" datetime type
     current_date = dtdt.now().date()
-#приведення заданої дати до типу datetime "тільки дата"
+# converting the specified date to the datetime type "date only"
     formated_date = dtdt.strptime(date, "%Y-%m-%d").date()
     delta = formated_date - current_date
     print (delta.days)
-#повернення різниці дат в форматі "тільки день"
+# returning the date difference in the "day only" format
     return delta.days
 
 get_days_from_today("2024-01-05")
