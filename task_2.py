858aa7446736d2f53b6c13f52f0594d61f7d2279
NazarS11 <bamperor@gmail.com>
2024-01-25 15:30:00 +0200

    v1.0

--- task_2.py
+++ task_2.py
@@ -1,3 +1,21 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    list = []
+# input validation, if at least 1 statement = True return empty list
+    if min < 1 or max > 1000 or quantity > max:
+        print (list)
+        return list
+    else:
+        while len(list) < quantity:
+            a = random.randint(min,max)
+# uniqueness check
+            if a not in list:
+# add unique random int number in the list
+                list.append(random.randint(min,max))
+# sort list
+        list.sort()
+        print(list)
+        return list
+
+get_numbers_ticket(0,50,6)
